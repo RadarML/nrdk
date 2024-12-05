c99319fe354cc6d5e33deda14ff4f252c461f572
Tianshu Huang <thetianshuhuang@gmail.com>
2024-12-05 14:36:18 -0500

    Clean up colors, flags

--- demo.py
+++ demo.py
@@ -6,9 +6,9 @@ import cv2
 import numpy as np
 import torch
 import yaml
-from matplotlib import colormaps
 from awr_api import AWRSystem
 from beartype.typing import cast
+from matplotlib import colormaps
 
 from deepradar import DeepRadar
 from demo.pipeline import ModelStream, ProcessingStream
@@ -17,6 +17,7 @@ from demo.pipeline import ModelStream, ProcessingStream
 def _parse():
     p = ArgumentParser(description="Evaluate radar model.")
 
+    p.add_argument("-t", "--type", help="Model output type.", default='depth')
     p.add_argument("-m", "--model", help="Path to model.")
     p.add_argument(
         "-k", "--checkpoint", default=None,
@@ -36,6 +37,7 @@ def _main(args):
 
     model = DeepRadar.load_from_experiment(
         args.model, checkpoint=args.checkpoint)
+    model.eval()
     model_stream = ModelStream(cast(DeepRadar, model))
     preproc_stream = ProcessingStream.from_config(
         transform=model.dataset["channels"]["radar"]["args"]["transform"])
@@ -43,21 +45,43 @@ def _main(args):
     out_stream = model_stream.apply(
         preproc_stream.apply(radar.qstream(numpy=True)))
 
+    if args.type == 'depth':
+        cmap = (np.array(colormaps['viridis'].colors) * 255).astype(np.uint8)
+    elif args.type == 'bev':
+        cmap = (np.array(colormaps['inferno'].colors) * 255).astype(np.uint8)
+    elif args.type == 'seg':
+        with open("schema/colors.yaml") as f:
+            cmap = np.array(yaml.load(f, Loader=yaml.SafeLoader)["colors"])
+    else:
+        raise ValueError(
+            f"Unknown model type: {args.type} "
+            "(must be `depth`, `bev`, or `seg`).")
+
+    window_name = f"grt:{args.type}"
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty(
+        window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
     ii = 0
-    viridis = (np.array(colormaps['viridis'].colors) * 255).astype(np.uint8)
     while True:
         frame = out_stream.get()
         if frame is None:
             break
         else:
-            # quant = ((frame['depth'] / 63) * 255).astype(np.uint8)
+            if args.type == 'depth':
+                quant = (
+                    np.clip(frame['depth'] / 63, 0, 1) * 255).astype(np.uint8)
+            elif args.type == 'bev':
+                quant = frame['bev']
+            else:
+                quant = frame['seg']
 
             quant = frame['bev']
-            img = np.take(viridis, quant, axis=0)
+            img = np.take(cmap, quant, axis=0)
             img = cv2.resize(
                 img, (1920, 1920 // 2), interpolation=cv2.INTER_NEAREST)
 
-            cv2.imshow('Depth', cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+            cv2.imshow(args.type, cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
             cv2.waitKey(10)
 
         ii += 1
